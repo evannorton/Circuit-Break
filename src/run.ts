78431983b377b80a4fefddb1e1b561fd4eb71646
Evan Norton <evan@retro-mmo.com>
2025-01-31 20:45:23 -0600

    Add main theme

--- src/run.ts
+++ src/run.ts
@@ -1,8 +1,14 @@
 import { createCollisionBoundaries } from "./functions/createCollisionBoundaries";
 import { createUI } from "./functions/ui/createUI";
-import { getCurrentTime, goToLevel, setPauseMenuCondition } from "pixel-pigeon";
+import {
+  getCurrentTime,
+  goToLevel,
+  playAudioSource,
+  setPauseMenuCondition,
+} from "pixel-pigeon";
 import { isGameOngoing } from "./functions/isGameOngoing";
 import { levelID } from "./constants";
+import { musicVolumeChannelID } from "./volumeChannels";
 import { state } from "./state";
 
 export const run = (): void => {
@@ -10,6 +16,10 @@ export const run = (): void => {
   createUI();
   goToLevel(levelID);
   createCollisionBoundaries();
+  playAudioSource("music/main", {
+    loopPoint: 70073,
+    volumeChannelID: musicVolumeChannelID,
+  });
   state.setValues({
     titleStartedAt: getCurrentTime(),
   });
