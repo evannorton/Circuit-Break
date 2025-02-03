0a33476b1886571bb9bf0772ec2561fb71db252c
Evan Norton <evan@retro-mmo.com>
2025-02-02 22:40:00 -0600

    Prettify instructions popup

--- src/functions/ui/createInstructions.ts
+++ src/functions/ui/createInstructions.ts
@@ -1,28 +1,41 @@
-import { createLabel, createQuadrilateral, getGameWidth } from "pixel-pigeon";
+import { createLabel, createSprite, getGameWidth } from "pixel-pigeon";
 import { state } from "../../state";
 
 export const createInstructions = (): void => {
-  const width: number = 216;
-  const height: number = 89;
+  const width: number = 218;
+  const height: number = 104;
   const x: number = Math.floor(getGameWidth() / 2 - width / 2);
   const y: number = 4;
   const condition = (): boolean =>
     state.values.gameStartedAt !== null &&
     state.values.enemiesStartedAt === null;
-  const textPadding: number = 4;
+  const textPadding: number = 8;
   const textX: number = x + textPadding;
   const textY: number = y + textPadding;
   const textMaxWidth: number = width - textPadding * 2;
-  createQuadrilateral({
-    color: "#000000",
+  createSprite({
+    animationID: "default",
+    animations: [
+      {
+        frames: [
+          {
+            height,
+            sourceHeight: height,
+            sourceWidth: width,
+            sourceX: 0,
+            sourceY: 0,
+            width,
+          },
+        ],
+        id: "default",
+      },
+    ],
     coordinates: {
       condition,
       x,
       y,
     },
-    height,
-    opacity: 0.7,
-    width,
+    imagePath: "tutorial",
   });
   createLabel({
     color: "#ffffff",
