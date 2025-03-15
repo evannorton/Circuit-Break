d4dd6901964f2d3229e031255dea987ffe2b1e2d
Evan Norton <evan@retro-mmo.com>
2025-03-15 02:20:28 -0500

    Allow shooting enemies to shoot from off screen

--- src/functions/landShootProjectiles.ts
+++ src/functions/landShootProjectiles.ts
@@ -6,7 +6,10 @@ import { shootHitboxWidth } from "../constants";
 export const landShootProjectiles = (): void => {
   for (const shootProjectile of getDefinables(ShootProjectile).values()) {
     const position: EntityPosition = getEntityPosition(shootProjectile.id);
-    if (position.x < -shootHitboxWidth || position.x > getGameWidth()) {
+    if (
+      position.x < -shootHitboxWidth - getGameWidth() ||
+      position.x > getGameWidth() * 2
+    ) {
       shootProjectile.remove();
     }
   }
